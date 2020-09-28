a49ecdde3ea21fc7ca90561d8596f34e1d49779e
saahil0826 <snawalkha0826@gmail.com>
2020-09-28 16:11:55 +0530

    navigation through withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css';
-import HomePage from './homepage.js'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.js";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
