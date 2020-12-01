6731079f5766fddba0701b429fe543e2747083a3
saahil0826 <snawalkha0826gmail.com>
2020-12-01 23:04:52 +0530

    Updates

---
+++ precache-manifest.1ec286747281f72f09fca2d758f438ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447f7532888b91d64faf917e7fb963c1",
+    "url": "/clothing-website/index.html"
+  },
+  {
+    "revision": "f5f9c4f0f6f6c2af5998",
+    "url": "/clothing-website/static/css/main.c11e3481.chunk.css"
+  },
+  {
+    "revision": "d7e62f8b82f7cf8b03b9",
+    "url": "/clothing-website/static/js/2.8aa06021.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/clothing-website/static/js/2.8aa06021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5f9c4f0f6f6c2af5998",
+    "url": "/clothing-website/static/js/main.8992af3f.chunk.js"
+  },
+  {
+    "revision": "77b363234e5c35bea169",
+    "url": "/clothing-website/static/js/runtime-main.64d46836.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing-website/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/clothing-website/static/media/shopping-bag.30f07071.svg"
+  }
+]);
