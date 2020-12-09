7dc2ef83acfcb274b4db62be8b991d04867dd1e3
saahil0826 <snawalkha0826gmail.com>
2020-12-09 17:13:14 +0530

    programatically adding shopData to firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import { createStructuredSelector } from 'reselect';
-
+import { createStructuredSelector } from "reselect";
 
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.js";
 import ShopPage from "./pages/shop/shop.js";
 import Header from "./components/header/header.js";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.js";
-import CheckoutPage from './pages/checkout/checkout.js';
-import { auth, createUserProfileDocument } from "./firebase/firebase.util.js";
+import CheckoutPage from "./pages/checkout/checkout.js";
+import {
+  auth,
+  createUserProfileDocument,
+  addCollectionAndDocuments
+} from "./firebase/firebase.util.js";
 import { setCurrentUser } from "./redux/user/user.actions.js";
-import { selectCurrentUser } from './redux/user/user.selectors.js';
-
+import { selectCurrentUser } from "./redux/user/user.selectors.js";
+import { selectCollectionsForPreview } from "./redux/shop/shop.selectors.js";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionsArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -34,6 +37,10 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+      addCollectionAndDocuments(
+        "collections",
+        collectionsArray.map(({ title, items }) => ({ title, items }))
+      );
     });
   }
 
@@ -46,10 +53,10 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-        <Redirect from="/clothing-website" to="/" />
+          <Redirect from="/clothing-website" to="/" />
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
@@ -68,7 +75,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyAVRPmrmx5r-coMet205z6EM-FIVcTLpyY",
@@ -33,18 +33,32 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
   return userRef;
 };
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
