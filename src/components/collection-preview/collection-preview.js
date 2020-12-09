940810b5937ec3bb2a2342d6da24ca707027ce79
saahil0826 <snawalkha0826gmail.com>
2020-12-10 00:20:32 +0530

    withSpinner HOC added

--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
 
 import CollectionItem from "../collection-item/collection-item.js";
 
 import "./collection-preview.scss";
 
-const CollectionPreview = ({ title, items, match }) => (
+const CollectionPreview = ({ title, items, history, match, routeName}) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <h1 className="title" onClick={() => history.push(`${match.path}/${routeName}`)}>{title.toUpperCase()}</h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
@@ -17,4 +18,4 @@ const CollectionPreview = ({ title, items, match }) => (
   </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
